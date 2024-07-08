61621652b46339c8916da30a88aed13c2306f5cc
Sunryeo <elma9700@gmail.com>
2024-07-08 22:11:51 +0900

    prac: refactoring

--- src/main/java/com/example/basic/UserRepositoryInterface.java
+++
@@ -1,5 +0,0 @@
-package com.example.basic;
-
-public interface UserRepositoryInterface {
-    void save(User user);
-}
--- src/main/java/com/example/basic/UserView.java
+++
@@ -1,9 +0,0 @@
-package com.example.basic;
-
-public class UserView {
-  public void printUserDetails(User user) {
-    System.out.println("User details");
-    System.out.println("User name: " + user.getName());
-    System.out.println("User email: " + user.getEmail());
-  }
-}
