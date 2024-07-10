d40b79aa076c01e065b9d93da4fef3887797c2b5
Sunryeo <elma9700@gmail.com>
2024-07-10 23:52:21 +0900

    prac: thymeleaf model addAttribute practice

--- src/main/java/com/example/day0709/ThymeleafController.java
+++ src/main/java/com/example/day0709/ThymeleafController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ThymeleafController {
   @GetMapping("/index")
   public String index(Model model) {
+    model.addAttribute("name", "hello spring!");
     return "index";
   }
 }
