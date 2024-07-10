a2a18daed7b8b80df1e21ba54ed832a688bbdf60
Sunryeo <elma9700@gmail.com>
2024-07-10 23:46:08 +0900

    prac: thymeleaf ssr basic practice

---
+++ src/main/java/com/example/day0709/ThymeleafController.java
@@ -0,0 +1,13 @@
+package com.example.day0709;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+  @GetMapping("/index")
+  public String index(Model model) {
+    return "index";
+  }
+}
