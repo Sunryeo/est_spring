44a4b65109ec286f33140a2766ecd5e592858226
Sunryeo <elma9700@gmail.com>
2024-08-13 21:07:57 +0900

    tdd simple test

---
+++ src/main/java/com/example/day0809_tdd_demo/Product.java
@@ -0,0 +1,11 @@
+package com.example.day0809_tdd_demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private int price;
+}
